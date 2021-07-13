43a19450c404ea2153e62097585b511e6250a070
hxie0 <xhy@chicagobooth.edu>
2021-07-13 23:38:09 +0000

    in progress

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
+    def blank_square_form
+        render({:template => "calculation_templates/square_form.html.erb"})
+    end
+
+    def calculate_square
+        # params = {"y1"=>"42"}
+        @num = params.fetch("y1")
+        @square_of_num = @num.to_i**2
+        render({:template => "calculation_templates/square_results.html.erb"})
+    end
 
 end
