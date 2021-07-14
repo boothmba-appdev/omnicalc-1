76710b43b91f4b2bd93054c09c55716c5babb77d
hxie0 <xhy@chicagobooth.edu>
2021-07-14 01:09:36 +0000

    almost done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
+    def blank_form
+        render({:template => "calculation_templates/new_form.html.erb"})
+    end
+
     def blank_square_form
         render({:template => "calculation_templates/square_form.html.erb"})
     end
@@ -6,8 +10,43 @@ class ApplicationController < ActionController::Base
     def calculate_square
         # params = {"y1"=>"42"}
         @num = params.fetch("y1")
-        @square_of_num = @num.to_i**2
+        @square_of_num = @num.to_f**2
         render({:template => "calculation_templates/square_results.html.erb"})
+    end
+
+    def blank_random_form
+        render({:template => "calculation_templates/random_form.html.erb"})
+    end
+
+    def calculate_random
+        # params = {"y1"=>"42"}
+        @lower = params.fetch("user_min").to_f
+        @upper = params.fetch("user_max").to_f
+        @result = rand(@lower..@upper)
+        render({:template => "calculation_templates/random_results.html.erb"})
+    end
+
+    def blank_square_root_form
+        render({:template => "calculation_templates/square_root_form.html.erb"})
+    end
+
+    def calculate_square_root
+        # params = {"y1"=>"42"}
+        @number = params.fetch("y2")
+        @square_root_num = @number.to_f**0.5
+        render({:template => "calculation_templates/square_root_results.html.erb"})
+    end
+
+    def blank_payment_form
+        render({:template => "calculation_templates/payment_form.html.erb"})
+    end
+
+    def calculate_payment
+        # params = {"y1"=>"42"}
+        #@lower = params.fetch("user_min").to_f
+        #@upper = params.fetch("user_max").to_f
+        #@result = rand(@lower..@upper)
+        render({:template => "calculation_templates/payment_results.html.erb"})
     end
 
 end
